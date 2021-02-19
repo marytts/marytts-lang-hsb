1b54b8d704a9046e1f5e36447aee60e36fd05c2a
Ingmar Steiner <steiner@coli.uni-saarland.de>
2021-02-19 14:42:19 +0100

    Refactor for parameterized testing

--- src/test/groovy/marytts/language/hsb/PreprocessTest.groovy
+++ src/test/groovy/marytts/language/hsb/PreprocessTest.groovy
@@ -1,13 +1,19 @@
 package marytts.language.hsb
 
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
 class PreprocessTest {
 
-    @Test
-    void testGetExpandedNumber() {
-        def input = 9
-        def expected = 'dźewjeć'
+    @DataProvider
+    Object[][] numbers() {
+        [
+                [9, 'dźewjeć']
+        ]
+    }
+
+    @Test(dataProvider = 'numbers')
+    void testGetExpandedNumber(Integer input, String expected) {
         def preprocess = new Preprocess()
         def actual = preprocess.getExpandedNumber(input)
         assert expected == actual
